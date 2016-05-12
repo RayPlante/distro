d1b4d5549b9f9097cb4930262ed707e4b4d6b368
Ray Plante <raymond.plante@nist.gov>
2016-05-12 11:40:27 -0400

    Item14069: SecurityTests: detects attach JS injection (version: passes if fixed)

--- UnitTestContrib/SecurityTests.pm
+++ UnitTestContrib/SecurityTests.pm
@@ -5,68 +5,42 @@ our @ISA = qw( FoswikiFnTestCase );
 
 # use strict;
 
-my $testWeb = 'Main'; #'TemporaryTestWeb'; # name of the test web
-my $testTopic = 'WebHome'; #'TestTopic';      # name of a topic
-my $session; # Foswiki instance
+my $session;    # Foswiki instance
 
 sub new {
     my $self = shift()->SUPER::new(@_);
-    #$self->{test_web} = $testWeb;
-    #$self->{test_topic} = $testTopic;
-    #$self->{test_user_login} = Foswiki::cfg{AdminUserLogin}
+
     return $self;
 }
 
 sub set_up {
-    my $this = shift; # the Test::Unit::TestCase object
+    my $this = shift;
     $this->SUPER::set_up();
     $session = undef;
 }
 
 sub create_session {
-    my $this = shift;
+    my $this       = shift;
     my $query_opts = shift;
-    # Make up a simple query
+
+    # a simple query using attach
     my $query = new Unit::Request($query_opts);
     $query->path_info("/$this->{test_web}/$this->{test_topic}");
     $query->action("attach");
 
     # Create a Foswiki instance
-    $session = $this->createNewFoswikiSession( $this->{test_user_login},$query );
+    $session =
+      $this->createNewFoswikiSession( $this->{test_user_login}, $query );
+
     return $session;
 }
 
-sub create_web_query {
-    my $this, $query_opts = shift;
-    
-    # Make up a simple query
-    my $query = new Unit::Request($query_opts);
-    # $query->path_info("/$this->{test_web}/$this->{test_topic}");
-    $query->action("attach");
-    my $response = new Unit::Response();
-    $response->charset("utf8");
-
-
-    # Create a Foswiki instance
-    $session = $this->createNewFoswikiSession( $this->{test_user_login},$query );
-    # $session = new Foswiki(undef, $query);
-
-    # and use it to create some test webs
-    # Need priveleged user to create root webs with Foswiki::Func.
-    Foswiki::Func::createWeb( $this->{test_web} );
-}
-
-
 sub tear_down {
-    my $this = shift; # the Test::Unit::TestCase object
+    my $this = shift;    # the Test::Unit::TestCase object
 
     if ($session) {
-        # This will erase the test webs
-        # $this->removeWebFixture( $session, $testWeb );
-        #$this->removeWebFixture( $session, $testUsersWeb );
 
-        # This will destroy the Foswiki instance.  We use eval to suppress errors
-        #eval { $session->finish() };
+        # FoswikiFnTestCase does most of this
         1;
     }
 
@@ -76,32 +50,33 @@ sub tear_down {
 
 sub test_setup {
 
-    # if this test fails, there may be something wrong with the design of 
+    # if this test fails, there may be something wrong with the design of
     # other tests testing real issues.
 
     my $this = shift;
 
-    $this->create_session({ filename => [ "goober" ] });
+    $this->create_session( { filename => ["goober"] } );
     my $query = $this->{request};
 
-    $this->assert_str_equals("attach", $query->action());
-    $this->assert_str_equals("filename=goober", $query->queryString());
-    $this->assert_str_equals("goober", scalar($query->param('filename')));
+    $this->assert_str_equals( "attach",          $query->action() );
+    $this->assert_str_equals( "filename=goober", $query->queryString() );
+    $this->assert_str_equals( "goober", scalar( $query->param('filename') ) );
 
     # print $query->url(-query => 1), "\n";
 
-    my($respText, $result, $stdout, $stderr) = $this->captureWithKey(
+    my ( $respText, $result, $stdout, $stderr ) = $this->captureWithKey(
         attach => sub {
             no strict 'refs';
             Foswiki::UI::Attach::attach( $this->{session} );
             use strict 'refs';
             $Foswiki::engine->finalize( $this->{session}{response},
-                                        $this->{session}{request} );
-        });
+                $this->{session}{request} );
+        }
+    );
 
     # print $respText, "\n";
 
-    $this->assert_matches(qr/<input [^>]* value="goober"/, $respText);
+    $this->assert_matches( qr/<input [^>]* value="goober"/, $respText );
 
 }
 
@@ -110,31 +85,34 @@ sub test_attach_filename_xss {
     my $this = shift;
 
     # send filename="><sCrIpT>alert(66562)</sCrIpT>
-    $this->create_session({ filename => [ '"><sCrIpT>alert(66562)</sCrIpT>' ] });
+    $this->create_session(
+        { filename => ['"><sCrIpT>alert(66562)</sCrIpT>'] } );
     my $query = $this->{request};
 
     # print $query->url(-query => 1), "\n";
 
-    my($respText, $result, $stdout, $stderr) = $this->captureWithKey(
+    my ( $respText, $result, $stdout, $stderr ) = $this->captureWithKey(
         attach => sub {
             no strict 'refs';
             Foswiki::UI::Attach::attach( $this->{session} );
             use strict 'refs';
             $Foswiki::engine->finalize( $this->{session}{response},
-                                        $this->{session}{request} );
-        });
+                $this->{session}{request} );
+        }
+    );
 
-    print $respText, "\n";
+    # print $respText, "\n";
 
     # our filename got it in in some form...
-    $this->assert_matches(qr/sCrIpT/, $respText,
-                          "Expected to see harmless trace of filename (sCrIpT)");
+    $this->assert_matches( qr/sCrIpT/, $respText,
+        "Expected to see harmless trace of filename (sCrIpT)" );
 
     # ...but must not allow pop-up alert
-    $this->assert_matches(qr/<sCrIpT>alert\(66562\)<\/sCrIpT>/, $respText,
-    #$this->assert_does_not_match(qr/<sCrIpT>alert\(66562\)<\/sCrIpT>/, $respText,
-                          "Detected Javascript injection: " .
-                          "<sCrIpT>alert\(66562\)<\/sCrIpT>");
+    $this->assert_does_not_match(
+        qr/<sCrIpT>alert\(66562\)<\/sCrIpT>/,
+        $respText,
+        "Detected Javascript injection: <sCrIpT>alert\(66562\)<\/sCrIpT>"
+    );
 
 }
 
